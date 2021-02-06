c15dd32d69a72356f37203e0c3672d20a6ed9f78
Anand krishna <ak0869@gmail.com>
2021-02-06 11:30:29 +0530

    Initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAJg_rDRASUkNy4i7f2Ig-WgKwEu55C8tk",
+    authDomain: "react42-16501.firebaseapp.com",
+    projectId: "react42-16501",
+    storageBucket: "react42-16501.appspot.com",
+    messagingSenderId: "138651019742",
+    appId: "1:138651019742:web:54cbbe58791b14b2d7154f"
+  },
 };
 
 /*
